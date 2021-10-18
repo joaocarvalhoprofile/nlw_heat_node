4923acbcf42bebc7cc19b37dc2cdafe367596624
Joao Carvalho <joaokvalho@gmail.com>
2021-10-18 20:50:27 -0300

    feat: Add route github oauth

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
+import "dotenv/config"
 import express from "express"
 
 const app = express()
 
-app.get('/', (request, response) => {
-  response.json({ server: "API Server 1.0" })
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  )
 })
 
 app.listen(4000, () => {
